b8eebab04727f452f043c72620da415a709f8706
Marc <marcmclara@gmail.com>
2025-11-10 00:02:17 +0100

    refactor: eliminar la propiedad distDir de la configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
-  distDir: 'out',
   basePath: '/Portfolio',
   assetPrefix: '/Portfolio/',
   trailingSlash: true,
